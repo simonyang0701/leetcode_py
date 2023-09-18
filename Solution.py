4111a98325863a8594ee127201861648a7d4ffb4
tianyu.yang <tianyu.yang@bytedance.com>
2023-09-18 14:29:23 -0700

    tyang: 2023/09/18 commit

--- Solution.py
+++ Solution.py
@@ -1,3 +1,6 @@
+from collections import defaultdict, deque
+
+
 class Solution(object):
     # 1
     def twoSum(self, nums, target):
@@ -15,3 +18,175 @@ class Solution(object):
             map[nums[i]] = i
 
         return []
+
+    # 22
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        answer = []
+        def backtracking(cur_string, left_count, right_count):
+            if len(cur_string) == 2 * n:
+                answer.append("".join(cur_string))
+                return
+            if left_count < n:
+                cur_string.append("(")
+                backtracking(cur_string, left_count + 1, right_count)
+                cur_string.pop()
+            if right_count < left_count:
+                cur_string.append(")")
+                backtracking(cur_string, left_count, right_count + 1)
+                cur_string.pop()
+        backtracking([], 0, 0)
+        return answer
+
+    # 56
+    # Time complexity: O(nlogn)
+    # Space complexity: O(n)
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort(key=lambda x: x[0])
+
+        merged = []
+        for interval in intervals:
+            # if the list of merged intervals is empty or if the current
+            # interval does not overlap with the previous, simply append it.
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+            # otherwise, there is overlap, so we merge the current and previous
+            # intervals.
+                merged[-1][1] = max(merged[-1][1], interval[1])
+
+        return merged
+
+    # 57
+    # Time complexity: O(N)
+    # Space complexity: O(1)
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+
+        for interval in intervals:
+            # the new interval is after the range of other interval, so we can leave the current interval baecause the new one does not overlap with it
+            if interval[1] < newInterval[0]:
+                result.append(interval)
+            # the new interval's range is before the other, so we can add the new interval and update it to the current one
+            elif interval[0] > newInterval[1]:
+                result.append(newInterval)
+                newInterval = interval
+            # the new interval is in the range of the other interval, we have an overlap, so we must choose the min for start and max for end of interval
+            elif interval[1] >= newInterval[0] or interval[0] <= newInterval[1]:
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(newInterval[1], interval[1])
+
+        result.append(newInterval)
+        return result
+
+    #787
+    # Time complexity: O(N+E*K)
+    # Space complexity: O(N+E*K)
+    def findCheapestPrice(self, n, flights, src, dst, k):
+        """
+        :type n: int
+        :type flights: List[List[int]]
+        :type src: int
+        :type dst: int
+        :type k: int
+        :rtype: int
+        """
+        graph, dis = defaultdict(list), [-1 for _ in range(n)]
+        for f, to, price in flights:
+            graph[f].append([to, price])
+
+        dis[src], q, step = 0, deque([src]), 0
+
+        while q and step <= k:
+            sz = len(q)
+            new_dis = list(dis)
+            for _ in range(sz):
+                cur = q.popleft()
+                for neighbor in graph[cur]:
+                    if new_dis[neighbor[0]] == -1 or new_dis[neighbor[0]] > dis[cur]+neighbor[1]:
+                        new_dis[neighbor[0]] = dis[cur] + neighbor[1]
+                        q.append(neighbor[0])
+            step += 1
+            dis = new_dis
+
+        return dis[dst]
+
+    # 1654
+    def minimumJumps(self, forbidden, a, b, x):
+        """
+        :type forbidden: List[int]
+        :type a: int
+        :type b: int
+        :type x: int
+        :rtype: int
+        """
+        limit = 2000 + a + b
+        visited = set(forbidden)
+        myque = deque([(0, True)]) # (pos, isForward)
+        hops = 0
+        while(myque):
+            l = len(myque)
+            while(l > 0):
+                l -= 1
+                pos, isForward = myque.popleft()
+                if pos == x:
+                    return hops
+                if pos in visited: continue
+                visited.add(pos)
+                if isForward:
+                    nxt_jump = pos - b
+                    if nxt_jump >= 0:
+                        myque.append((nxt_jump, False))
+                nxt_jump = pos + a
+                if nxt_jump <= limit:
+                    myque.append((nxt_jump, True))
+            hops += 1
+        return -1
+
+    # 2556
+    # Time complexity: O(n*m)
+    # Space complexity: O(n*m)
+    def isPossibleToCutPath(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: bool
+        """
+        m, n = len(grid), len(grid[0])
+
+        #  number of paths from (0, 0) to (i, j)
+        dp1 = [[0] * (n + 1) for _ in range(m + 1)]
+        dp1[1][1] = 1
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if grid[i - 1][j - 1]:
+                    dp1[i][j] += dp1[i - 1][j] + dp1[i][j - 1]
+
+        #  number of paths from (i, j) to (m-1, n-1)
+        dp2 = [[0] * (n + 1) for _ in range(m + 1)]
+        dp2[-2][-2] = 1
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if grid[i][j]:
+                    dp2[i][j] += dp2[i + 1][j] + dp2[i][j + 1]
+
+        # number of paths from (0, 0) to (m-1, n-1)
+        target = dp1[-1][-1]
+
+        for i in range(m):
+            for j in range(n):
+                if (i != 0 or j != 0) and (i != m - 1 or j != n - 1):
+                    if dp1[i + 1][j + 1] * dp2[i][j] == target:
+                        return True
+        return False
--- test_solution.py
+++ test_solution.py
@@ -4,10 +4,53 @@ from Solution import Solution
 
 solution = Solution()
 
-
 def test_twoSum():
     nums = [2, 7, 11, 15]
     target = 9
     res = solution.twoSum(nums, target)
     print(res)
     assert res == [0, 1]
+
+def test_isPossibleToCutPath():
+    grid = [[1, 1, 1], [1, 0, 0], [1, 1, 1]]
+    res = solution.isPossibleToCutPath(grid)
+    print(res)
+    assert res is True
+
+def test_findCheapestPrice():
+    n = 4
+    flights = [[0, 1, 100], [1, 2, 100], [2, 0, 100], [1, 3, 600], [2, 3, 200]]
+    src = 0
+    dst = 3
+    k = 1
+    res = solution.findCheapestPrice(n, flights, src, dst, k)
+    print(res)
+    assert res == 700
+
+def test_minimumJumps():
+    forbidden = [14, 4, 18, 1, 15]
+    a = 3
+    b = 15
+    x = 9
+    res = solution.minimumJumps(forbidden, a, b, x)
+    print(res)
+    assert res == 3
+
+def test_generateParenthesis():
+    n = 3
+    res = solution.generateParenthesis(n)
+    print(res)
+    assert res == ["((()))", "(()())", "(())()", "()(())", "()()()"]
+
+def test_merge():
+    intervals = [[1,3],[2,6],[8,10],[15,18]]
+    res = solution.merge(intervals)
+    print(res)
+    assert res == [[1,6],[8,10],[15,18]]
+
+def test_insert():
+    intervals = [[1,3],[6,9]]
+    newInterval = [2,5]
+    res = solution.insert(intervals, newInterval)
+    print(res)
+    assert res == [[1,5],[6,9]]
