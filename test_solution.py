3a40b97b4138d11f88b0ccdc179a54e972eea606
tianyu.yang <tianyu.yang@bytedance.com>
2025-01-17 21:56:37 -0800

    ty: 88

--- test_solution.py
+++ test_solution.py
@@ -17,3 +17,12 @@ def test_addTwoNumbers():
     output = solution.addTwoNumbers(l1, l2)
     print_listnode(output)
     assert listnode_to_list(output) == [7, 0, 8]
+
+def test_merge():
+    nums1 = [1, 2, 3, 0, 0, 0]
+    m = 3
+    nums2 = [2, 5, 6]
+    n = 3
+    solution.merge(nums1, m, nums2, n)
+    print(nums1)
+    assert nums1 == [1,2,2,3,5,6]
